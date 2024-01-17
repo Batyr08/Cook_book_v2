d7f8514cb2c7c6400a689ce99c02b674e7dd170d
DarkGreyRabbit <darkgreyrabbit@mail.ru>
2024-01-17 21:26:23 +0300

    Стили

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -6,12 +6,18 @@ function Layout({ children, login }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/css/style.css" />
+        <link rel="stylesheet" href="/css/navbar.css" />
         <title>Cookbook blog</title>
       </head>
       <body>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
-          <div className="container-fluid">
-            <div className="collapse navbar-collapse" id="navbarNav">
+    
+      <div className="glass" />
+      <div className="logo_container">
+        <input type="checkbox" id="nav-cta" />
+        <aside>
+          <label htmlFor="nav-cta" className="cta"><span /></label>
+           
               {login ? (
                 <ul className="navbar-nav">
                   <li className="nav-item">
@@ -59,11 +65,19 @@ function Layout({ children, login }) {
                   </li>
                 </ul>
               )}
-            </div>
-          </div>
-        </nav>
+            
+            </aside>
+      </div>
+    
         {children}
       </body>
+    
+      <div className="custom-shape-divider-bottom-1705101024">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill" />
+        </svg>
+      </div>
+     
     </html>
   );
 }
