31d330a3fda93eb48a5a46f46077c6bf46de8670
DarkGreyRabbit <darkgreyrabbit@mail.ru>
2024-01-18 15:50:32 +0300

    Изменения

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -4,7 +4,18 @@ const Layout = require('./Layout');
 function Details({ login, recipe }) {
   return (
     <Layout login={login}>
-    <p>{recipe.title}</p>
+        <link rel="stylesheet" href="/css/details.css" />
+    <div className="detailscontainer">
+        <div className='divimg'><img src={recipe.image} alt="" /></div>
+
+        <div className='information'> 
+        <h1>{recipe.title}</h1>
+        <div><img src="/img/clock.png" alt="" />{recipe.time} мин.</div>
+        <div><img src="/img/ingredients.png" alt="" />{recipe.ingredient}</div>
+        <p>Рецепт: {recipe.description}</p>
+        
+        </div>
+    </div>
     </Layout>
   );
 }
